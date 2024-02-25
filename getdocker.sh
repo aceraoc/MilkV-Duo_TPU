7a511fe22be20a66f0db6bd9d36bf4a38e5656b4
Shirakami <74444260+aceraoc@users.noreply.github.com>
2024-02-25 16:53:47 +0800

    Update getdocker.sh
    update file name of cvitek_mlir_ubuntu-18.04_v1.5.0-890-g6e3b61411.tar.gz

--- getdocker.sh
+++ getdocker.sh
@@ -1,5 +1,5 @@
 wget --user='cvitek_mlir_2022' --password='Bxc~b6n!Zn' --timeout=5 --waitretry=0 --tries=3 --retry-connrefused  ftp://218.17.249.213/home/tpu_rel_v1.5.0-868-g743c9d53a/cvitek_tpu_sdk_cv180x_musl_riscv64_rvv.tar.gz  -P .
 wget --user='cvitek_mlir_2022' --password='Bxc~b6n!Zn' --timeout=5 --waitretry=0 --tries=3 --retry-connrefused  ftp://218.17.249.213/home/tpu_rel_v1.5.0-868-g743c9d53a/cvimodel_samples_cv180x.tar.gz  -P .
 wget --user='cvitek_mlir_2022' --password='Bxc~b6n!Zn' --timeout=5 --waitretry=0 --tries=3 --retry-connrefused  ftp://218.17.249.213/home/docker_cvitek_dev_1.9-ubuntu-18.04.tar  -P .
-wget --user='cvitek_mlir_2022' --password='Bxc~b6n!Zn' --timeout=5 --waitretry=0 --tries=3 --retry-connrefused  ftp://218.17.249.213/home/cvitek_mlir_ubuntu-18.04_v1.5.0-883-gee0cbe9e3.tar.gz  -P .
+wget --user='cvitek_mlir_2022' --password='Bxc~b6n!Zn' --timeout=5 --waitretry=0 --tries=3 --retry-connrefused  ftp://218.17.249.213/home/cvitek_mlir_ubuntu-18.04_v1.5.0-890-g6e3b61411.tar.gz  -P .
 wget --user='cvitek_mlir_2022' --password='Bxc~b6n!Zn' --timeout=5 --waitretry=0 --tries=3 --retry-connrefused  ftp://218.17.249.213/home/CVITEK_TPU快速入门指南.pdf  -P .
